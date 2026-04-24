ad1b06ee7697f593bf20d8f6825aa0d82ab169a8
o9n <no_reply@email.com>
2026-04-24 10:45:50 +0700

    [nvim]/fix-telescope

--- .config/nvim/lua/o9n/plugin/telescope.lua
+++ .config/nvim/lua/o9n/plugin/telescope.lua
@@ -41,7 +41,7 @@ end
 
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  branch = "master",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
